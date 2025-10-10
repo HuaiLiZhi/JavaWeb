74c7b214356675fb6e857b9c0e7d3812a167f5ac
huailizhi <a1487351283@qq.com>
2025-10-10 11:44:25 +0800

    46

---
+++ web-ai-project01/springboot-web-01/src/main/java/com/huailizhi/controller/UserController.java
@@ -0,0 +1,39 @@
+package com.huailizhi.controller;
+
+import cn.hutool.core.io.IoUtil;
+import com.huailizhi.pojo.User;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    @RequestMapping("/list")
+    public List<User> list() throws FileNotFoundException {
+        //加载并读取resources中的user.txt文件，获取用户数据
+        InputStream in = this.getClass().getClassLoader().getResourceAsStream("user.txt");
+        ArrayList<String> lines = IoUtil.readLines(in, StandardCharsets.UTF_8, new ArrayList<>());
+
+        //解析用户信息，封装为User对象 -> list集合
+        List<User> userList = lines.stream().map(line -> {
+            String[] split = line.split(",");
+            Integer id = Integer.parseInt(split[0]);
+            String username = split[1];
+            String password = split[2];
+            String name = split[3];
+            Integer age = Integer.parseInt(split[4]);
+            LocalDateTime updateTime = LocalDateTime.parse(split[5], DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+            return new User(id, username, password, name, age, updateTime);
+        }).toList();
+
+        //返回json数据
+        return userList;
+    }
+}
