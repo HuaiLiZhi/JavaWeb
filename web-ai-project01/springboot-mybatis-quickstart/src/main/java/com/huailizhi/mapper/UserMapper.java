c89d456d109b1157e5937521f11c772a610114ae
huailizhi <a1487351283@qq.com>
2025-10-15 09:54:17 +0800

    74

--- web-ai-project01/springboot-mybatis-quickstart/src/main/java/com/huailizhi/mapper/UserMapper.java
+++ web-ai-project01/springboot-mybatis-quickstart/src/main/java/com/huailizhi/mapper/UserMapper.java
@@ -1,14 +1,53 @@
 package com.huailizhi.mapper;
 
 import com.huailizhi.pojo.User;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 
 import java.util.List;
 
 @Mapper
 public interface UserMapper {
 
-    @Select("select * from user")
+
+
+    /**
+     * 查询所有用户信息
+     * @return 用户列表
+     */
+//    @Select("select * from user")
     public List<User> findAll();
+
+    /**
+     * 根据ID删除用户信息
+     * @param id 用户ID
+     */
+    @Delete("delete from user where id = #{id}")
+    public Integer deleteById(Integer id);
+
+
+    /**
+     * 插入用户信息
+     * @param user 用户对象，包含用户名、密码、姓名和年龄
+     * @return 插入成功的记录数
+     */
+    @Insert("insert into user(username, password, name, age) values (#{username}, #{password}, #{name}, #{age})")
+    public Integer insert(User user);
+
+
+    /**
+     * 根据ID更新用户信息
+     * @param user 用户对象，包含需要更新的用户信息和用户ID
+     */
+    @Update("update user set username=#{username}, password=#{password}, name=#{name}, age=#{age} where id=#{id}")
+    public void updateById(User user);
+
+
+    /**
+     * 根据用户名和密码查询用户信息
+     * @param username 用户名
+     * @param password 密码
+     * @return 匹配的用户对象，如果未找到则返回null
+     */
+    @Select("select * from user where username=#{username} and password=#{password}")
+    public User findByUsernameAndPassword(@Param("username") String username, @Param("password") String password);
 }
--- web-ai-project01/springboot-mybatis-quickstart/src/test/java/com/huailizhi/SpringbootMybatisQuickstartApplicationTests.java
+++ web-ai-project01/springboot-mybatis-quickstart/src/test/java/com/huailizhi/SpringbootMybatisQuickstartApplicationTests.java
@@ -22,4 +22,37 @@ class SpringbootMybatisQuickstartApplicationTests {
         }
     }
 
+    @Test
+    public void testDeleteById(){
+        userMapper.deleteById(5);
+    }
+
+
+    @Test
+    public void testInsert(){
+        User user = new User();
+        user.setUsername("huailizhi");
+        user.setPassword("123456");
+        user.setName("Huailizhi");
+        user.setAge(18);
+        userMapper.insert(user);
+    }
+
+    @Test
+    public void testUpdateById(){
+        User user = new User();
+        user.setId(6);
+        user.setUsername("huailizhi");
+        user.setPassword("123456");
+        user.setName("怀里之");
+        user.setAge(18);
+        userMapper.updateById(user);
+    }
+
+    @Test
+    public void testFindByUsernameAndPassword(){
+        User user = userMapper.findByUsernameAndPassword("huailizhi", "123456");
+        System.out.println(user);
+    }
+
 }
