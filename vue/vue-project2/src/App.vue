33de89fc801400f5aaf0dda3cca0ba5b0c63e28a
怀里之 <a1487351283@qq.com>
2025-11-12 10:50:17 +0800

    159

--- vue/vue-project2/src/App.vue
+++ vue/vue-project2/src/App.vue
@@ -1,9 +1,10 @@
 <script setup>
-import ElementDemo from "@/views/ElementDemo.vue";
+// import ElementDemo from "@/views/ElementDemo.vue";
+import EmpList from "@/views/EmpList.vue";
 </script>
 
 <template>
-  <ElementDemo></ElementDemo>
+  <EmpList></EmpList>
 </template>
 
 <style scoped>
