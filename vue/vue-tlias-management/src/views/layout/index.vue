f5cc8cffb9d255237e931e0946755e44a382afc2
怀里之 <a1487351283@qq.com>
2025-11-13 13:16:21 +0800

    161

---
+++ vue/vue-tlias-management/src/views/layout/index.vue
@@ -0,0 +1,116 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="common-layout">
+    <el-container>
+      <!-- Header 区域 -->
+      <el-header class="header">
+        <span class="title">Tlias智能学习辅助系统</span>
+        <span class="right_tool">
+          <a href="">
+            <el-icon><EditPen /></el-icon> 修改密码 &nbsp;&nbsp;&nbsp; |  &nbsp;&nbsp;&nbsp;
+          </a>
+          <a href="">
+            <el-icon><SwitchButton /></el-icon> 退出登录
+          </a>
+        </span>
+      </el-header>
+      
+      <el-container>
+        <!-- 左侧菜单 -->
+        <el-aside width="200px" class="aside">
+          <el-menu :default-openeds="['1', '3']">
+            <el-sub-menu index="1">
+              <template #title>
+                <el-icon><message /></el-icon>Navigator One
+              </template>
+              <el-menu-item-group>
+                <template #title>Group 1</template>
+                <el-menu-item index="1-1">Option 1</el-menu-item>
+                <el-menu-item index="1-2">Option 2</el-menu-item>
+              </el-menu-item-group>
+              <el-menu-item-group title="Group 2">
+                <el-menu-item index="1-3">Option 3</el-menu-item>
+              </el-menu-item-group>
+              <el-sub-menu index="1-4">
+                <template #title>Option4</template>
+                <el-menu-item index="1-4-1">Option 4-1</el-menu-item>
+              </el-sub-menu>
+            </el-sub-menu>
+            <el-sub-menu index="2">
+              <template #title>
+                <el-icon><icon-menu /></el-icon>Navigator Two
+              </template>
+              <el-menu-item-group>
+                <template #title>Group 1</template>
+                <el-menu-item index="2-1">Option 1</el-menu-item>
+                <el-menu-item index="2-2">Option 2</el-menu-item>
+              </el-menu-item-group>
+              <el-menu-item-group title="Group 2">
+                <el-menu-item index="2-3">Option 3</el-menu-item>
+              </el-menu-item-group>
+              <el-sub-menu index="2-4">
+                <template #title>Option 4</template>
+                <el-menu-item index="2-4-1">Option 4-1</el-menu-item>
+              </el-sub-menu>
+            </el-sub-menu>
+            <el-sub-menu index="3">
+              <template #title>
+                <el-icon><setting /></el-icon>Navigator Three
+              </template>
+              <el-menu-item-group>
+                <template #title>Group 1</template>
+                <el-menu-item index="3-1">Option 1</el-menu-item>
+                <el-menu-item index="3-2">Option 2</el-menu-item>
+              </el-menu-item-group>
+              <el-menu-item-group title="Group 2">
+                <el-menu-item index="3-3">Option 3</el-menu-item>
+              </el-menu-item-group>
+              <el-sub-menu index="3-4">
+                <template #title>Option 4</template>
+                <el-menu-item index="3-4-1">Option 4-1</el-menu-item>
+              </el-sub-menu>
+            </el-sub-menu>
+          </el-menu>
+        </el-aside>
+        
+        <el-main>
+          右侧核心展示区域
+        </el-main>
+      </el-container>
+      
+    </el-container>
+  </div>
+</template>
+
+<style scoped>
+.header {
+  background-image: linear-gradient(to right, #00547d, #007fa4, #00aaa0, #00d072, #a8eb12);
+}
+
+.title {
+  color: white;
+  font-size: 40px;
+  font-family: 楷体;
+  line-height: 60px;
+  font-weight: bolder;
+}
+
+.right_tool{
+  float: right;
+  line-height: 60px;
+}
+
+a {
+  color: white;
+  text-decoration: none;
+}
+
+.aside {
+  width: 220px;
+  border-right: 1px solid #ccc;
+  height: 730px;
+}
+</style>
