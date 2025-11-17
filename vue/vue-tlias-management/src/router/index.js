187efe5d3eb22974ece45f0fbfd6156dc74825e0
huailizhi <a1487351283@qq.com>
2025-11-17 17:38:09 +0800

    163

--- vue/vue-tlias-management/src/router/index.js
+++ vue/vue-tlias-management/src/router/index.js
@@ -6,8 +6,8 @@ import DeptView from '@/views/dept/index.vue'
 import EmpView from '@/views/emp/index.vue'
 import LogView from '@/views/log/index.vue'
 import LoginView from '@/views/login/index.vue'
-import ReportEmpView from '@/views/report/emp/index.vue'
-import ReportStuView from '@/views/report/stu/index.vue'
+import EmpReportView from '@/views/report/emp/index.vue'
+import StuReportView from '@/views/report/stu/index.vue'
 import StuView from '@/views/stu/index.vue'
 import LayoutView from '@/views/layout/index.vue'
 
@@ -15,16 +15,23 @@ import LayoutView from '@/views/layout/index.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    { path: '/index', name: 'index', component: IndexView },
-    { path: '/clazz', name: 'clazz', component: ClazzView },
-    { path: '/dept', name: 'dept', component: DeptView },
-    { path: '/emp', name: 'emp', component: EmpView },
-    { path: '/log', name: 'log', component: LogView },
+    {
+      path: '/',
+      name: '',
+      component: LayoutView,
+      redirect: '/index',
+      children: [
+        { path: 'index', name: 'index', component: IndexView },
+        { path: 'clazz', name: 'clazz', component: ClazzView },
+        { path: 'dept', name: 'dept', component: DeptView },
+        { path: 'emp', name: 'emp', component: EmpView },
+        { path: 'log', name: 'log', component: LogView },
+        { path: 'empReport', name: 'empReport', component: EmpReportView },
+        { path: 'stuReport', name: 'stuReport', component: StuReportView },
+        { path: 'stu', name: 'stu', component: StuView },
+      ]
+    },
     { path: '/login', name: 'login', component: LoginView },
-    { path: '/report/emp', name: 'reportEmp', component: ReportEmpView },
-    { path: '/report/stu', name: 'reportStu', component: ReportStuView },
-    { path: '/stu', name: 'stu', component: StuView },
-    { path: '/', name: 'layout', component: LayoutView },
   ]
 })
 
--- vue/vue-tlias-management/src/views/dept/index.vue
+++ vue/vue-tlias-management/src/views/dept/index.vue
@@ -1,11 +1,40 @@
 <script setup>
+import { ref } from "vue";
 
+const tableData = ref([
+  {
+    "id": 1,
+    "name": "学工部",
+    "createTime": "2022-09-01T23:06:29",
+    "updateTime": "2022-09-01T23:06:29"
+  }
+])
 </script>
 
 <template>
-  部门管理
+  <h1>部门管理</h1>
+  <div class="container">
+    <el-button type="primary">+ 新增部门</el-button>
+  </div>
+
+  <div class="container">
+    <el-table :data="tableData" border style="width: 100%">
+      <el-table-column type="index" label="序号" width="100" align="center"/>
+      <el-table-column prop="name" label="部门名称" width="260" align="center"/>
+      <el-table-column prop="updateTime" label="最后操作时间" width="300" align="center"/>
+      <el-table-column label="操作" align="center">
+        <template #default="scope">
+          <el-button type="primary" size="small"><el-icon><EditPen /></el-icon>编辑</el-button>
+          <el-button type="danger" size="small"><el-icon><Delete /></el-icon>删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
+
 </template>
 
 <style scoped>
-
+.container {
+  margin: 10px 0px;
+}
 </style>
